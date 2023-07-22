6f5b7dfab5c5ed141490340817698d92a3452552
Kirara02 <fathul.sirnaraja92@gmail.com>
2023-07-22 13:58:26 +0700

    initial commit

---
+++ lib/src/features/home/view/home_page.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        actions: const [],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10.0),
+        child: const Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
